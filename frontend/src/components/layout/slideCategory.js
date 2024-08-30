00e70309a93dd25592286ff774b0287ec6345496
duc viet <viettmelo@gmail.com>
2024-08-30 12:03:52 +0700

    ducviet

---
+++ frontend/src/components/layout/slideCategory.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class slideCategory extends Component {
+    render() {
+        return (
+            <div>
+                ducviet
+            </div>
+        );
+    }
+}
+
+export default slideCategory;
